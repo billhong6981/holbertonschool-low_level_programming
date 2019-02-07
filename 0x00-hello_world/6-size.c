124b04d48319e844cb2bd43a62957a02622527d3
billhong6981 <bill.burton6981@gmail.com>
2019-02-07 22:31:10 +0000

    6-size2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 /**
  * main - Entry point
  *
- * The function will output the type size of 32bits machine
- * or the type size of 64bits machine
- *
+ * The function will output the type size of in 32bits environment
+ * or the type size of in 64bits environment
+ * compile it in 64 bits with no warning
+ * compile it in 32 bits with waring
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char : %d byte(s)\n", sizeof(char));
-printf("Size of a int : %d byte(s)\n", sizeof(int));
-printf("Size of a long int : %d byte(s)\n", sizeof(long));
-printf("Size of a long long int : %d byte(s)\n", sizeof(double));
-printf("Size of a float : %d byte(s)\n", sizeof(float));
+printf("Size of a char : %lu byte(s)\n", sizeof(char));
+printf("Size of a int : %lu byte(s)\n", sizeof(int));
+printf("Size of a long int : %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int : %lu byte(s)\n", sizeof(double));
+printf("Size of a float : %lu byte(s)\n", sizeof(float));
 
 return (0);
 
