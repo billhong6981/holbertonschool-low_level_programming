97d4396e5b46012c20b0b9eb1dea89e96084cde6
billhong6981 <bill.burton6981@gmail.com>
2019-02-07 18:43:43 +0000

    4-puts.c5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-  char *a = "\"Programming is like building a multilingual puzzle\n";
+  char *a = "\"Programming is like building a multilingual puzzle";
 
   puts(a);
   return (0);
