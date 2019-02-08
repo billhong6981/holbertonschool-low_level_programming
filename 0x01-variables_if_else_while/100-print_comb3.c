2264d33b5bdce3942d42939eaebb6502b2bfbd4f
billhong6981 <bill.burton6981@gmail.com>
2019-02-08 23:43:48 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0' ; a <= '8' ; )
+	{
+		for (b = '0' ; b <= '9' ; )
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				b++;
+			}
+			else
+			{
+				b++;
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
