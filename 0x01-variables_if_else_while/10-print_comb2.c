1c449fb3ab93dff7def8e2bfd8e737f231be1e71
billhong6981 <bill.burton6981@gmail.com>
2019-02-08 21:04:33 +0000

    10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 0 ; a <= 99 ; a++)
+{
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
