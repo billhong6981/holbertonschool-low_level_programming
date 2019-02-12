941debf5cb1b9d055eb9914ac9bd809673b4fa5b
billhong6981 <bill.burton6981@gmail.com>
2019-02-12 23:33:29 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,9 @@ int print_last_digit(int i)
 {
 	int last_digit;
 
-	if (i < 0)
-		last_digit = ((-1) * i) % 10;
-	else
-		last_digit = i % 10;
+	last_digit = i % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
