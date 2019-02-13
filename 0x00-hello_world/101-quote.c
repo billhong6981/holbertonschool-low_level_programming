8e9f290199335dc0cd4bb64919a7c5051ef820bf
billhong6981 <bill.burton6981@gmail.com>
2019-02-13 08:23:14 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print a string
+ *
+ * Return: 1 on success.
+ */
+
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (i = 0; i < 58; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (1);
+}
