26e283d762a0c466aaf54f6bc8f76cd334e802a3
billhong6981 <bill.burton6981@gmail.com>
2019-02-19 05:56:56 +0000

    6-puts2

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - a function that prints one character out of two in one string.
+ * @str: a pointer point to the string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
