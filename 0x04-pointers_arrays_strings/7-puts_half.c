fa88ec2ffb0d5d38d1244e03586b0f5cea4bf81d
billhong6981 <bill.burton6981@gmail.com>
2019-02-19 06:16:16 +0000

    7-puts

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *  puts_half - a function that prints half of a string.
+ * @str: a pointer point to a string.
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	j = i / 2;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
