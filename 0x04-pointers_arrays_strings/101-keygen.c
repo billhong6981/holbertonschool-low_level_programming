f09c2af451af570111d9e4595b86a43908a5955e
billhong6981 <bill.burton6981@gmail.com>
2019-02-20 06:37:30 +0000

    keygen

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 /**
- * _keygen - the program generate a 4 digits of random number as password
+ * main - the program generate a 9 digits of random number as password
+ * @n: a random number
+ * @snum: a array of string that covert by numbers n
+ * @fp: a pointer point to file crackme.
  *
- * Return: return a random numbers of string.
+ * Return: return 0.
  */
 
 int main(void)
 {
-	int n;
+	int i, n, tens;
+	char snum[30];
+	FILE *fp;
 
+	tens = 100000000;
+	printf("Open crackme file in write mode\n");
+	fp = fopen("101-crackme", "w");
+	if (fp == NULL)
+	{
+		printf("Could not open file test.c");
+		return (1);
+	}
+	printf("\n Generate a random numbers for the password \n");
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	n = n % 10000;
-	return (n);
+	n = (rand() - RAND_MAX / 2);
+	for (i = 0; i < 9 && tens > 0; i++)
+	{
+		snum[i] = (n / tens) % 10 + '0';
+		tens /= 10;
+	}
+	snum[i++] = '\0';
+	fputs(snum, fp);
+	fputs("\n", fp);
+	printf("Closing the file");
+	fclose(fp);
+	return (0);
 }
