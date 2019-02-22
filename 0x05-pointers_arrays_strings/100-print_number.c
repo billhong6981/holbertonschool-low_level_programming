d6f56753e68dc381d7a6234694ffc654de79b511
billhong6981 <bill.burton6981@gmail.com>
2019-02-22 02:42:42 +0000

    print numbers

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_number - a function that prints a integer
+ * @n: a integer number to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int temp1, temp2;
+	int i, j, tens;
+
+	i = 1;
+	tens = 1;
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	temp1 = n;
+	temp2 = n;
+	while (temp1 >= 10)
+	{
+		temp1 = temp1 / 10;
+		i++;
+	}
+	for (j = 1; j < i; j++)
+		tens = tens * 10;
+	while (tens > 1)
+	{
+		_putchar((temp2 / tens) % 10 + '0');
+		tens /= 10;
+	}
+	_putchar(temp2 % 10 + '0');
+}
