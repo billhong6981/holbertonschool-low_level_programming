eb131b84c38db3c5260841c5feccde98f7b00a57
billhong6981 <bill.burton6981@gmail.com>
2019-02-22 02:13:02 +0000

    cap again1

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -2,29 +2,21 @@
 
 /**
  * cap_string - a function that capitalizes each word in a string.
- * @str: a pointer point to a string
+ * @s: a pointer point to a string
  *
  * Return: return a pointer that point to the changed string.
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
-	char delimitor[] = " \t\n,;.!?\"(){}";
+	char del[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; delimitor[j] != '\0'; j++)
-		{
-			if (str[i] == delimitor[j])
-			{
-				i++;
-				if (str[i] >= 'a' && str[i] <= 'z')
-					str[i] -= 32;
-				else
-					j = 0;
-			}
-		}
+		for (j = 0; del[j] != '\0'; j++)
+			if (s[i - 1] == del[j] && s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
 	}
-	return (str);
+	return (s);
 }
