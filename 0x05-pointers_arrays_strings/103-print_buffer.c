fd1020446f2c66c6a8ead860bd08e2caba9a41d4
billhong6981 <bill.burton6981@gmail.com>
2019-02-23 01:01:23 +0000

    print buffer

--- 0x05-pointers_arrays_strings/103-print_buffer.c
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -14,18 +14,21 @@ void print_buffer(char *b, int size)
 	int i, j, add;
 
 	j = add = 0;
-	for (i = 0; i < size - 1 && b[i] != 0; i++)
+	for (i = 0; i < size / 10 && b[i] != 0; i++)
 	{
 		add += j;
 		printf("%.8x: ", add);
 		for (j = 0; j < 10; )
 		{
-			printf("%x%x ", b[i * 10 + j], b[i * 10 + j + 1]);
+			printf("%.2x%.2x ", b[i * 10 + j], b[i * 10 + j + 1]);
 			j += 2;
 		}
 		for (j = 0; j < 10; j++)
 		{
-			printf("%c", *(b + i * 10 + j));
+			if (b[i * 10 + j] == '\0' || b[i * 10 + j] == '\x2')
+				printf(".");
+			else
+				printf("%c", b[i * 10 + j]);
 		}
 		printf("\n");
 	}
