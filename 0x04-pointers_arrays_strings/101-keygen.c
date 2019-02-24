908bb5cad64bb70d7189ef32609b78bae8974d8f
billhong6981 <bill.burton6981@gmail.com>
2019-02-24 01:09:22 +0000

    keygen

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -6,38 +6,31 @@
 /**
  * main - the program generate a 9 digits of random number as password
  * @n: a random number
- * @snum: a array of string that covert by numbers n
- * @fp: a pointer point to file crackme.
+ * @sum: a array of floor string that cover all alphabet character.
+ * @i: a char array index.
  *
  * Return: return 0.
  */
 
 int main(void)
 {
-	int i, n, tens;
-	char snum[30];
-	FILE *fp;
+	int i, n;
+	unsigned long int sum;
+	char password[100];
 
-	tens = 100000000;
-	printf("Open crackme file in write mode\n");
-	fp = fopen("101-crackme", "w");
-	if (fp == NULL)
+	n = i = sum = 0;
+	char al[63] = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	srand(time(NULL));
+	while (sum < (2772 - 122))
 	{
-		printf("Could not open file test.c");
-		return (1);
+		n = rand() % 62;
+		password[i] = al[n];
+		sum = sum + password[i];
+		i++;
 	}
-	printf("\n Generate a random numbers for the password \n");
-	srand(time(0));
-	n = (rand() - RAND_MAX / 2);
-	for (i = 0; i < 9 && tens > 0; i++)
-	{
-		snum[i] = (n / tens) % 10 + '0';
-		tens /= 10;
-	}
-	snum[i++] = '\0';
-	fputs(snum, fp);
-	fputs("\n", fp);
-	printf("Closing the file");
-	fclose(fp);
+	n = 2772 - sum;
+	password[i] = n;
+	password[++i] = '\0';
+	printf("%s\n", password);
 	return (0);
 }
