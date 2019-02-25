1c4a0055083dad74816e96550e08c1dbd642aed0
billhong6981 <bill.burton6981@gmail.com>
2019-02-25 19:07:39 +0000

    search character

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - a function that locates a character in string.
+ * @s: a pointer point to string
+ * @c: a constant character search for.
+ *
+ * Return: the pointer that the character at the string (in success) or NULL for not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (s);
+}
