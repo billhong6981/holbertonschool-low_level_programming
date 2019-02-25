196b5d9150ba828b226725edf8db6c514a3482c6
billhong6981 <bill.burton6981@gmail.com>
2019-02-25 22:17:00 +0000

    2-strcr

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 			return (s);
-		else if (*(s + 1) == c)
-			return (s + 1);
 		s++;
 	}
-	return (s + 1);
+	return (s);
 }
