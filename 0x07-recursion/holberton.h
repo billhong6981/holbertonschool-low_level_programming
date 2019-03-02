6cd9963f6f2bc24ffa04220fa6e52493343aa112
billhong6981 <bill.burton6981@gmail.com>
2019-03-02 02:08:12 +0000

    wild compare

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -11,6 +11,5 @@ int wildcmp(char *s1, char *s2);
 int natural_root(int x, int n);
 int _base_number(int base, int n);
 int compare_str(char *str, int i, int len);
-int find_wildcard(char *str);
-int compare_wildcard(char *str1, char *str2);
-int compare_no_wildcard(char *str1, char *str2);
+char *_find_wildcard(char *str);
+char *_compare_wildcard(char *str1, char *str2);
