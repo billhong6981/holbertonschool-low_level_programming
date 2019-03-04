16ec8bf15746d23954b536e842fb5b039f5feb66
billhong6981 <bill.burton6981@gmail.com>
2019-03-04 02:11:53 +0000

    3-mill

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -18,8 +17,6 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	else
-	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
+	return (0);
 }
