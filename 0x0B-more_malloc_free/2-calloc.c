5366ff632145c8be110191fd216b2753439ea009
billhong6981 <bill.burton6981@gmail.com>
2019-03-08 04:37:00 +0000

    add

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -15,11 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(size * nmemb + 1);
+	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb ; i++)
 		p[i] = 0;
-	p[nmemb] = '\0';
-	return (p);
+	return ((char *)p);
 }
