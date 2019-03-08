d13015cc73f1d5e9770f7d8f3cad16f21da31b66
billhong6981 <bill.burton6981@gmail.com>
2019-03-08 05:13:34 +0000

    3-array

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -16,6 +16,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	ptr = malloc(sizeof(*ptr) * (max - min + 1));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < (max - min + 1); i++)
 		ptr[i] = min + i;
 	return (ptr);
