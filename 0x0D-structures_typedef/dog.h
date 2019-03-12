557e0a5498459381a49a9998820f3df4c0ac03f9
billhong6981 <bill.burton6981@gmail.com>
2019-03-12 02:14:14 +0000

    add1

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,19 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - data structure of dog
  * @name: name of the dog type of pointer
  * @age: age of the dog type of int
  * @owner: name of the owner of the dog, pointer
  *
- * Description: Defines the dog informations and type of dog's struct
+ * Description: Defines the dog informations and type of dog's data struct
  */
-typedef struct dog {
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+/* Additional function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 char *_strdup(char *str);
