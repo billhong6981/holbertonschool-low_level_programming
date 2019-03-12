ee60bce36eadc7f152b2e8b8f9dfc28f87a62f43
billhong6981 <bill.burton6981@gmail.com>
2019-03-12 02:03:12 +0000

    add

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - a function builds dogs informations
+ * @name: a dog name as string
+ * @age: a dog age in float type
+ * @owner: a dog's owner as string
+ *
+ * Return: return the pointer to new_dog, NULL if fail
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *your_dog;
+        your_dog = malloc(sizeof(dog_t));
+        your_dog->name = _strdup(name);
+	your_dog->age = age;
+        your_dog->owner = _strdup(owner);
+        if (your_dog == NULL || your_dog->name == NULL || your_dog->owner == NULL)
+	{
+                free(your_dog);
+		return (NULL);
+        }
+	else
+		return (your_dog);
+}
+
+/**
+ * _strdup - a function that copies a string into a new buffer
+ * @str: a pointer point to the string
+ *
+ * Return: pointer point to the buffer
+ */
+
+char *_strdup(char *str)
+{
+char *p;
+unsigned int len, j;
+
+if (str == NULL)
+	return (NULL);
+for (len = 0; str[len] != '\0'; len++)
+	;
+len++;
+if (len < 1)
+	return (NULL);
+p = malloc(len);
+if (p == NULL)
+{
+free(p);
+return (NULL);
+}
+for (j = 0; j < len; j++)
+	p[j] = str[j];
+p[j] = '\0';
+return (p);
+}
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data structure of dog
+ * @name: name of the dog type of pointer
+ * @age: age of the dog type of int
+ * @owner: name of the owner of the dog, pointer
+ *
+ * Description: Defines the dog informations and type of dog's struct
+ */
+typedef struct dog {
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */
