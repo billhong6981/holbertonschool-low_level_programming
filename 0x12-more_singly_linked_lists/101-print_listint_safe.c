26d461aa35f4585fef4407e49cc4fc76b52bd10b
billhong6981 <bill.burton6981@gmail.com>
2019-03-27 04:03:11 +0000

    fix output

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,26 +12,22 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	size_t count1 = 0;
-	const listint_t *hare, *tortoise, *current;
-	int flag = 0;
+	const listint_t *hare, *tortoise;
 
 	if (head == NULL)
 		exit(98);
-	else
+	count++;
+	tortoise = head;
+	hare = head->next;
+	while (hare != NULL && hare != tortoise)
 	{
+		hare = hare->next;
 		count++;
-		tortoise = head;
-		hare = head->next;
-		while (hare != NULL && hare != tortoise)
+		if (hare != NULL && hare != tortoise)
 		{
-			hare = hare->next;
 			count++;
-			if (hare != NULL && hare != tortoise)
-			{
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
+			tortoise = tortoise->next;
+			hare = hare->next;
 		}
 	}
 	if (hare == NULL)
@@ -45,19 +41,12 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	else
 	{
-		current = head;
-		for (count1 = 0; current != tortoise; count1++)
-			current = current->next;
-		while (1)
+		for (count1 = 0; count1 <= (count / 2 + 2); count1++)
 		{
-			if (head == tortoise && flag == 1)
-				break;
-			if (head == tortoise)
-				flag = 1;
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
 		printf("-> [%p] %d\n", (void *)head, head->n);
-		return (count / 2 + 1);
+		return (count / 2 + 3);
 	}
 }
