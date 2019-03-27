5fc7b3442c5a6b052a09cd47594ba443917d7d79
billhong6981 <bill.burton6981@gmail.com>
2019-03-27 02:20:33 +0000

    fix checker

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current = *head;
 	unsigned int i = 0;
 
-	if ((head == NULL) || (*head == NULL && idx != 0))
+	if (head == NULL)
 		return (NULL);
 	if (idx == 0)
 		(*head) = add_nodeint(head, n);
@@ -29,5 +29,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 			add_nodeint(&(current->next), n);
 	}
-	return (*head);
+	return ((*head));
 }
