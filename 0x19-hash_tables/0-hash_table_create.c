9192e61c20cd9df948641f7fb3e2b5bcd200dc54
billhong6981 <bill.burton6981@gmail.com>
2019-06-04 22:48:37 +0000

    tasks

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,20 +2,20 @@
 /**
  * hash_table_create - a function that creates hash table
  * @size: size of table
- * Return: return the address of the hash table
+ * Return: return the address of the hash table on success, otherwise NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_t;
 	hash_node_t **ar;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 	h_t = malloc(sizeof(hash_table_t));
 	if (!h_t)
 		return (NULL);
 	h_t->size = size;
-	ar = malloc(sizeof(hash_node_t) * 1024);
+	ar = malloc(sizeof(hash_node_t *) * 1024);
 	if (!ar)
 	{
 		free(h_t);
