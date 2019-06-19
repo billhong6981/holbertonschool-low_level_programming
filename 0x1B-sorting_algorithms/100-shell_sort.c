4a177f3a06a9c96a09051c74a5fb4accd3e70dd7
billhong6981 <bill.burton6981@gmail.com>
2019-06-19 20:47:02 +0000

    tasks

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -23,7 +23,7 @@ void swap(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t k = 0, i, j, gap;
+	size_t k = 0, i, j, gap, flag;
 	size_t gaps[] = {364, 121, 40, 13, 4, 1, 0};
 
 	if (size < 2)
@@ -36,17 +36,20 @@ void shell_sort(int *array, size_t size)
 	}
 	for (gap = gaps[k]; gap > 0; gap = gaps[k++])
 	{
+		flag = 0;
 		for (i = gap; i < size; i++)
 		{
 			for (j = i - gap; array[j] > array[j + gap]; )
 			{
 				swap(&array[j], &array[j + gap]);
+				flag = 1;
 				if (j >= gap)
 					j = j - gap;
 				else
 					break;
 			}
 		}
-		print_array(array, size);
+		if (flag == 1)
+			print_array(array, size);
 	}
 }
